54cfcc1c86b7ba6a8ac994ca05692ebc7b230910
pollei <stephen.pollei@gmail.com>
2025-07-23 23:55:56 -0700

    recursively makes html from md

--- src/main.py
+++ src/main.py
@@ -5,7 +5,8 @@ import publish
 def main():
     publish.clean_public()
     publish.publish_static()
-    publish.generate_page("content/index.md", "template.html", "public/index.html")
+    #publish.generate_page("content/index.md", "template.html", "public/index.html")
+    publish.generate_pages_recursive()
     #tn = TextNode("This is some anchor text", "link", "https://www.boot.dev")
     #tn = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
     #print(tn)
--- src/publish.py
+++ src/publish.py
@@ -17,7 +17,7 @@ from md_to_html import markdown_to_html_node
 h1_pat = re.compile( r"^#\s(.*)$", re.M )
 def extract_title(markdown):
     mtch = h1_pat.search(markdown)
-    print(f"{markdown=} {mtch=}")
+    #print(f"{markdown=} {mtch=}")
     if mtch: return mtch.group(1).strip()
     raise Exception("extract_title: title in markdown is required")
 
@@ -86,10 +86,10 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown)
     html_nodes = markdown_to_html_node(markdown)
     html_txt = html_nodes.to_html()
-    print(f"{html_txt=}")
+    #print(f"{html_txt=}")
     out_content = template.replace("{{ Title }}", title)
     out_content = out_content.replace("{{ Content }}", html_txt)
-    print(f"{out_content=}")
+    #print(f"{out_content=}")
     dest_dir = os.path.dirname(dest_path)
     os.makedirs(dest_dir, exist_ok=True)
     try:
@@ -103,4 +103,30 @@ def generate_page(from_path, template_path, dest_path):
         print(f"generate_page failed write {dest_path=} {from_path=} {err=}")
 
 
+def generate_pages_recursive(dir_path_content="content", template_path="template.html", dest_dir_path="public"):
+    dir_lst, file_lst = [], []
+    try:
+        scan_iter = os.scandir(dir_path_content)
+        for dir_entry in scan_iter:
+            if dir_entry.is_symlink(): contunue
+            if dir_entry.is_dir(follow_symlinks=False):
+                dir_lst.append(dir_entry.name)
+                continue
+            if dir_entry.is_file(follow_symlinks=False):
+                file_lst.append(dir_entry.name)
+                continue
+    except Exception as err:
+        print(f"generate_pages_recursive failed scan {dest_dir_path=} {dir_path_content=} {err=}")
+    finally:
+        scan_iter.close()
+    for file_name in file_lst:
+        if not file_name.endswith(".md"): continue
+        path_md_content = os.path.join(dir_path_content , file_name)
+        path_html_output = os.path.join(dest_dir_path, file_name[:-3] + ".html")
+        generate_page(path_md_content, template_path, path_html_output)
+    for dir_name in dir_lst:
+            to_new = os.path.join(dest_dir_path, dir_name)
+            from_new = os.path.join(dir_path_content , dir_name)
+            print(f"enerate_pages_recursiv new dir {to_new=} {from_new=}")
+            generate_pages_recursive(from_new, template_path, to_new)
 
